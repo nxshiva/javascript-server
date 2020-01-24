8b4124927c92e2ed02b8a5122114d05e5859b0dc
Shiva Sharma <shiva.sharma@lapr242.successivetech.com>
2020-01-24 09:51:02 +0530

    refactor:create funtion to print the pattern

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,5 +1,36 @@
-let num=process.argv[2];
-if(isNaN(num)){
+let num=process.argv[2]
+function check(number){
+    if(isNaN(number) || number<=2 || number>=10){
+        return false;
+    }
+    return true;
+}
+function diamond(number){
+for(let i=0;i<number;i++) {
+for(let j = number-1; j > i; j--){
+process.stdout.write(" ");
+}
+
+for(let k=0;k<=i;k++){
+process.stdout.write("* ");
+}
+
+process.stdout.write('\n');
+}
+
+for(let i = number; i > 0; i--) {
+for(j = number; j > i; j--){
+process.stdout.write(" ");
+}
+
+for(let k = 0; k < i; k++){
+process.stdout.write("* ");
+}
+
+process.stdout.write('\n');
+}
+}
+function askAgain(){
     const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
@@ -7,103 +38,22 @@ const rl = readline.createInterface({
 });
 
     rl.question("Enter a number between 2 and 10: ", function(number) {
-        
-for(let i=0;i<number;i++) {
-    
-    for(let j = number-1; j > i; j--){
-        process.stdout.write(" ");
-    }
-
-    for(let k=0;k<=i;k++){
-        process.stdout.write("* ");
-    }
-
-    process.stdout.write('\n');
-}
-
-for(let i = number; i > 0; i--) {
-    for(j = number; j > i; j--){
-        process.stdout.write(" ");
-    }
-
-    for(let k = 0; k < i; k++){
-        process.stdout.write("* ");
-    }
-
-    process.stdout.write('\n');
-}
+       if(check(number)){
+           diamond(number)
+           rl.close();
+       }
+       else{
         rl.close();
+        askAgain();
+       }
     });
-
-rl.on("close", function() {
-    process.exit(0);
-});
 }
-else if(num<=2 || num>10){
-    const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-    rl.question("Enter a number between 2 and 10: ", function(number) {
-        
-for(let i=0;i<number;i++) {
-    
-    for(let j = number-1; j > i; j--){
-        process.stdout.write(" ");
+function diamondPattern(num){
+    if(check(num)){
+        diamond(num)
     }
-
-    for(let k=0;k<=i;k++){
-        process.stdout.write("* ");
+    else{
+        askAgain()
     }
-
-    process.stdout.write('\n');
 }
-
-for(let i = number; i > 0; i--) {
-    for(j = number; j > i; j--){
-        process.stdout.write(" ");
-    }
-
-    for(let k = 0; k < i; k++){
-        process.stdout.write("* ");
-    }
-
-    process.stdout.write('\n');
-}
-        rl.close();
-    });
-
-rl.on("close", function() {
-    process.exit(0);
-});
-}
-else
-{
-   
-for(let i=0;i<num;i++) {
-    
-    for(let j = num-1; j > i; j--){
-        process.stdout.write(" ");
-    }
-
-    for(let k=0;k<=i;k++){
-        process.stdout.write("* ");
-    }
-
-    process.stdout.write('\n');
-}
-
-for(let i = num; i > 0; i--) {
-    for(j = num; j > i; j--){
-        process.stdout.write(" ");
-    }
-
-    for(let k = 0; k < i; k++){
-        process.stdout.write("* ");
-    }
-
-    process.stdout.write('\n');
-}
-}
+diamondPattern(num);
--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,5 +1,22 @@
-let num=process.argv[2];
-if(isNaN(num)){
+let num=process.argv[2]
+function check(number){
+    if(isNaN(number) || number<=2 || number>=10){
+        return false;
+    }
+    return true;
+}
+function equilateral(num){
+    for(let i=0;i<num;i++){
+        for(let j=i;j<num-1;j++){
+            process.stdout.write(" ")
+        }
+        for(let k=0;k<=i;k++){
+            process.stdout.write("* ")
+        }
+        process.stdout.write("\n")
+    }
+}
+function askAgain(){
     const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
@@ -7,62 +24,22 @@ const rl = readline.createInterface({
 });
 
     rl.question("Enter a number between 2 and 10: ", function(number) {
-        for(let i = 0; i < number; i++){
-            for(let j = number-1; j>i; j--){
-                process.stdout.write(" ");
-            }
-        
-            for(let k = 0; k <= i; k++){
-                process.stdout.write("* ");
-            }
-        
-            process.stdout.write('\n');
-        }
+       if(check(number)){
+           equilateral(number)
+           rl.close();
+       }
+       else{
         rl.close();
+        askAgain();
+       }
     });
-
-rl.on("close", function() {
-    process.exit(0);
-});
 }
-
-else{
-    if(num<=2 || num>10){
-        const readline = require("readline");
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    
-        rl.question("Enter a number between 2 and 10: ", function(number) {
-            for(let i = 0; i < number; i++){
-                for(let j = number-1; j>i; j--){
-                    process.stdout.write(" ");
-                }
-            
-                for(let k = 0; k <= i; k++){
-                    process.stdout.write("* ");
-                }
-            
-                process.stdout.write('\n');
-            }
-            rl.close();
-        });
-    
-    rl.on("close", function() {
-        process.exit(0);
-    });
+function equilateralPattern(num){
+    if(check(num)){
+        equilateral(num)
     }
     else{
-    for(let i = 0; i < num; i++){
-        for(let j = num-1; j>i; j--){
-            process.stdout.write(" ");
-        }
-    
-        for(let k = 0; k <= i; k++){
-            process.stdout.write("* ");
-        }
-    
-        process.stdout.write('\n');
-    }}
+        askAgain()
+    }
 }
+equilateralPattern(num);
