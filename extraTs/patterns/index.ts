b5ad20baf869fe63edb0b1c4434757428c6df1bf
Vikas Chauhan <vikas.chauhan@successive.tech>
2020-03-19 09:18:02 +0530

    Revert "Develop(Backend)"

--- extraTs/patterns/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as diamondPattern } from './diamond';
-export { default as equilateralPattern } from './equilateral';
--- src/controllers/user/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserController } from './Controller';
-export { default as userRouter } from './routes';
